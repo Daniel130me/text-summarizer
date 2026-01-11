90d52ef9ac0271383126d995233b4a547095f9f7
Daniel130me <kosokodaniel@gmail.com>
2026-01-11 02:00:42 +0100

    update for deployment

--- server/server.js
+++ server/server.js
@@ -3,29 +3,35 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import apiRoutes from "./routes/api.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", apiRoutes);
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/dist")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-	});
-}
+app.use("/api", apiRoutes);
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
 mongoose
   .connect(MONGO_URI)
   .then(() => {
-    console.log("mongoDB Connected");
-    app.listen(PORT, () => console.log(`running on port the ${PORT}`));
+    console.log("MongoDB connected");
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
   })
-  .catch((err) => console.error("mongoDB onnection on Error:", err));
+  .catch((err) => console.error("MongoDB connection error:", err));
